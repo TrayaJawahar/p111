7752c17093fdc70130a07f62e879ce71f3ea6eda
TrayaJawahar <72916948+TrayaJawahar@users.noreply.github.com>
2021-10-26 15:07:44 +0530

    Add files via upload

---
+++ p.py
@@ -0,0 +1,61 @@
+import plotly.figure_factory as ff 
+import plotly.graph_objects as go 
+import statistics
+import random 
+import pandas as pd 
+import csv
+
+df = pd.read_csv("datap.csv")
+data=df["reading_time"].tolist()
+population_mean=statistics.mean(data)
+
+def random_set_of_mean(counter):
+    dataset = []
+    for i in range(0, counter):
+        random_index= random.randint(0,len(data)-1)
+        value = data[random_index]
+        dataset.append(value)
+    mean = statistics.mean(dataset)
+    return mean
+
+# Pass the number of time you want the mean of the data points as a parameter in range function in for loop
+mean_list = []
+for i in range(0,1000):
+    set_of_means= random_set_of_mean(30)
+    mean_list.append(set_of_means)
+
+
+## calculating mean and standard_deviation of the sampling distribution.
+std_deviation = statistics.stdev(mean_list)
+
+first_std_deviation_start, first_std_deviation_end = mean-std_deviation, mean+std_deviation
+second_std_deviation_start, second_std_deviation_end = mean-(2*std_deviation), mean+(2*std_deviation)
+third_std_deviation_start, third_std_deviation_end = mean-(3*std_deviation), mean+(3*std_deviation)
+print("std1",first_std_deviation_start, first_std_deviation_end)
+print("std2",second_std_deviation_start, second_std_deviation_end)
+print("std3",third_std_deviation_start,third_std_deviation_end)
+
+fig = ff.create_distplot([dice_result], ["Result"], show_hist=False)
+fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.17], mode="lines", name="MEAN"))
+fig.add_trace(go.Scatter(x=[first_std_deviation_start, first_std_deviation_start], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 1"))
+fig.add_trace(go.Scatter(x=[first_std_deviation_end, first_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 1"))
+fig.add_trace(go.Scatter(x=[second_std_deviation_start, second_std_deviation_start], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2"))
+fig.add_trace(go.Scatter(x=[second_std_deviation_end, second_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2"))
+fig.show()
+
+df = pd.read_csv("datap.csv")
+data = df["reading_time"].tolist()
+mean_of_sample1 = statistics.mean(data)
+print("Mean of sample1:- ",mean_of_sample1)
+fig = ff.create_distplot([mean_list], ["time"], show_hist=False)
+fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.17], mode="lines", name="MEAN"))
+fig.add_trace(go.Scatter(x=[mean_of_sample1, mean_of_sample1], y=[0, 0.17], mode="lines", name="MEAN OF SAMPLE"))
+fig.add_trace(go.Scatter(x=[first_std_deviation_end, first_std_deviation_end], y=[0, 0.17], mode="lines"))
+fig.show()
+
+
+z_score = (mean1 - mean_of_sample1)/std_deviation
+print("The z score is = ",z_score)
+
+
+
